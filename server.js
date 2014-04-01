e91bbd5f77ef57121400cc041036e1701674e9de
David Nichol <david_r_nichol@yahoo.com>
2014-03-31 21:43:22 -0400

    Adding jade views

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ var app = express();
 
 app.use(logfmt.requestLogger());
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "jade");
+app.use(express.static(__dirname + "/public"));
 app.get('/', function(req, res) {
-  res.send("Dave's home page!");
+  res.render("index", { title: "Home" });
 });
-
+console.log(__dirname);
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
