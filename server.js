e6d35cb949349ce3b8bd123f8fe1de341df37413
David Nichol <david_r_nichol@yahoo.com>
2015-08-02 21:57:12 -0400

    Added pics images and random image picker

--- server.js
+++ server.js
@@ -2,15 +2,37 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var fs = require("fs");
 
 app.use(logfmt.requestLogger());
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "jade");
 app.use("/public", express.static("public"));
-app.get('/', function(req, res) {
+app.get("/", function(req, res) {
   res.render("index", { title: "Home" });
 });
+app.get("/random", function(req, res) {
+  var picsDir = __dirname + "/pics";
+  fs.readdir(picsDir, function(err, files) {
+    if(err) {
+        console.log(err);
+        res.status(err.status).end();
+    }
+
+    var fileIndex = Math.floor((Math.random() * files.length) + 1);
+    var fileName = picsDir + "/" + files[fileIndex];
+    res.sendFile(fileName, {}, function(err) {
+      if(err) {
+        console.log(err);
+        res.status(err.status).end();
+      }
+    });
+  });
+
+  
+});
+
 console.log(__dirname);
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
